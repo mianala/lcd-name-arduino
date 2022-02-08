1a3fa3d7e567a54023ba01efc02b1dd606adbfc2
Loharano <mianalaloharano@gmail.com>
2022-02-08 15:26:42 +0300

    working

--- blue.ino
+++ blue.ino
@@ -1,26 +1,30 @@
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 #include <string.h>
 
 // LCD Pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-// Global Var
-#define contra 9
-#define bri 10
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
+
 String Incomming_values = "";
 
 void setup()
 {
-  lcd.begin(16, 2);
-  pinMode(contra, OUTPUT);
-  pinMode(bri, OUTPUT);
-  digitalWrite(contra, LOW);
-  analogWrite(bri, 255);
+
+  lcd.init(); // initialize the lcd
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Mianala");
+  Serial.begin(9600);
 }
 void loop()
 {
   if (Serial.available() > 0)
   {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+
     Incomming_values = Serial.readString();
     lcd.print(Incomming_values);
 
@@ -29,7 +33,6 @@ void loop()
     lcd.print("100% accurate");
 
     delay(5000);
-    digitalWrite(contra, LOW);
     lcd.setCursor(0, 0);
   }
 }
